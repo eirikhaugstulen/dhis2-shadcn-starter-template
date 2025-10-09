e730230aa095b7b40e2a4c23c8239a3ce5506418
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-10-09 02:13:09 +0200

    feat: update configuration and localization files
    - Added application name and title to the configuration file for better identification.
    - Updated the POT creation and revision dates in the localization file to reflect recent changes.
    - Introduced application title and description context in the localization file for improved clarity.

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@dhis2/cli-app-scripts').D2Config} */
 const config = {
     type: 'app',
+    name: 'assisted-ai-development',
+    title: 'AI-assisted Development',
 
     entryPoints: {
         app: './src/app.tsx',
